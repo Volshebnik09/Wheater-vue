ed7091ae42e512884f8cb3ca5dbdd5833e1a94f5
volshebnik <shulgin.grigoriy@yandex.ru>
2022-07-27 23:09:35 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/volshebnik09.github.io/wheater-vue/'
 })
